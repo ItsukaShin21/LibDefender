a3d9dae523a1b8281c32fba9da2cc3ef8499144f
ItsukaShin21 <jef.lotoc@gmail.com>
2024-04-08 15:07:30 +0800

    Added a modal for registering a user and function for it

--- UserListPage.xaml.cs
+++ UserListPage.xaml.cs
@@ -1,36 +1,96 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Data;
+using System.Numerics;
+using System.Windows;
 using System.Windows.Controls;
+using MySql.Data.MySqlClient;
+
 
 namespace LibDefender
 {
     public partial class UserListPage : Page
     {
+        readonly string connectionString = DatabaseConfig.systemDatabase;
+
         public UserListPage()
         {
             InitializeComponent();
-            PopulateDataGrid();
             this.DataContext = this;
         }
-        private void PopulateDataGrid()
-        {
-            var dummyData = new ObservableCollection<DummyItem4>(new[]
-            {
-                new DummyItem4 { UserRfid = 121, Username = "John Doe", Email = "example@gmail.com", Password = "asdsad", UserType = "Admin" },
-                new DummyItem4 { UserRfid = 131, Username = "Alice Smith", Email = "example@gmail.com", Password = "ewrv", UserType = "Library Staff" },
-                new DummyItem4 { UserRfid = 141, Username = "Bob Johnson", Email = "example@gmail.com", Password = "rtyjfb", UserType = "Library Staff" }
-            });
 
-            // Set the ItemsSource of the DataGrid to the collection
-            DataGrid.ItemsSource = dummyData;
+        private void UsersListQuery(string userListQuery)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            using var command = new MySqlCommand(userListQuery, connection);
+
+            connection.Open();
+
+            DataTable newData = new();
+
+            using MySqlDataAdapter dataAdapter = new(command);
+            dataAdapter.Fill(newData);
+
+            UsersList.ItemsSource = newData.DefaultView;
         }
-    }
-    public class DummyItem4
-    {
-        public int? UserRfid { get; set; }
-        public string? Username { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-        public string? UserType { get; set; }
+
+        private void UserDeleteQuery(string userRfid)
+        {
+            try
+            {
+                using var connection = new MySqlConnection(connectionString);
+                using var command = new MySqlCommand("DELETE FROM users WHERE userRfid = @userRfid", connection);
+                command.Parameters.AddWithValue("@userRfid", userRfid);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                // Handle or log the exception as appropriate
+                MessageBox.Show($"Error deleting user: {ex.Message}");
+            }
+        }
+
+        public void RefreshUsersList()
+        {
+            string userListQuery = "SELECT * FROM users";
+            UsersListQuery(userListQuery);
+        }
+
+        private void UserListPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            RefreshUsersList();
+        }
+
+        private void DeleteButton_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (sender is Button button && button.DataContext is DataRowView dataContext)
+            {
+                var userRfid = dataContext["userRfid"].ToString();
+                var userName = dataContext["username"].ToString();
+
+                // Ask for user confirmation
+                var result = MessageBox.Show($"Are you sure you want to delete {userName}?", "Confirm Deletion", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.Yes)
+                {
+                    UserDeleteQuery(userRfid!);
+                    RefreshUsersList();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error: Unable to retrieve data for deletion.");
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            var adminWindow = Application.Current.Windows.OfType<AdminWindow>().FirstOrDefault();
+
+            if (adminWindow != null)
+            {
+                adminWindow.Blur.Visibility = Visibility.Visible;
+            }
+            UserRegisterModal.Instance.ShowDialog();
+        }
     }
 }
---
+++ UserRegisterModal.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Numerics;
+using System.Windows;
+using System.Windows.Input;
+using MySql.Data.MySqlClient;
+
+namespace LibDefender
+{
+    public partial class UserRegisterModal : Window
+    {
+        private static UserRegisterModal? newInstance;
+        readonly string connectionString = DatabaseConfig.systemDatabase;
+
+        public static UserRegisterModal Instance
+        {
+            get
+            {
+                if (newInstance == null)
+                {
+                    newInstance = new UserRegisterModal();
+                    newInstance.Closed += (s, e) => newInstance = null;
+                }
+                return newInstance;
+            }
+        }
+
+        public UserRegisterModal()
+        {
+            InitializeComponent();
+            this.Owner = Application.Current.MainWindow as AdminWindow;
+        }
+
+        private void RegisterUserQuery(string query, BigInteger userRfid, string username, string email, string password, string userType)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            using var command = new MySqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@userRfid", userRfid);
+            command.Parameters.AddWithValue("@username", username);
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@password", password);
+            command.Parameters.AddWithValue("@userType", userType);
+
+            connection.Open();
+
+            int result = Convert.ToInt32(command.ExecuteScalar());
+
+            if (result == 0)
+            {
+                this.Close();
+                MessageBox.Show($"{username} has been successfully registered!");
+                connection.Close();
+
+                var adminWindow = Application.Current.Windows.OfType<AdminWindow>().FirstOrDefault();
+
+                if (adminWindow != null)
+                {
+                    adminWindow.Blur.Visibility = Visibility.Hidden;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Registration failed!");
+
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            var adminWindow = Application.Current.Windows.OfType<AdminWindow>().FirstOrDefault();
+
+            if (adminWindow != null)
+            {
+                adminWindow.Blur.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            BigInteger userRfid = BigInteger.Parse(rfidTxtBox.Text);
+            string username = usernameTxtBox.Text;
+            string email = emailTxtBox.Text;
+            string password = passwordTxtBox.Text;
+            string userType = userTypeTxtBox.Text;
+
+            string query = "INSERT INTO users (userRfid, username, email, password, userType)" +
+                "VALUES (@userRfid, @username, @email, @password, @userType)";
+
+            RegisterUserQuery(query, userRfid, username, email, password, userType);
+        }
+    }
+}
